f44cdd6bdaf1719a0ef91c8143047a7bb1182966
Janika2210 <sakethram2323@gmail.com>
2025-09-02 16:59:44 +0530

    Delete FaceRecognition directory

--- FaceRecognition/task3.py
+++
@@ -1,8 +0,0 @@
-import logging
-logging.basicConfig(filename="recognition.log", level=logging.INFO)
-
-def log_result(name, distances):
-    if name:
-        logging.info(f"[MATCH] {name} | Distances: {distances}")
-    else:
-        logging.info("[NO MATCH] No recognized face in frame.")
--- FaceRecognition/task7.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_encoding(encoding):
-    """
-    Convert a 128-dim face encoding into a SHA256 hash string.
-    (For future secure storage)
-    """
-    return hashlib.sha256(encoding.tobytes()).hexdigest()
